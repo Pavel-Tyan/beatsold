4a5164b951d084799925097380fdc17b20c7cd53
Pavel Tyan <paveltyan032@gmail.com>
2021-07-24 15:49:42 +0600

    3week

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fullscreenMenu = document.querySelector('.fullscreen-menu');
-const fullscreenMenuOpenBtn = document.querySelector('#burger-btn')
+const fullscreenMenuOpenBtn = document.querySelector('#burger-btn');
 
 fullscreenMenuOpenBtn.addEventListener('click', e => {
     fullscreenMenu.style.display = 'flex';
@@ -10,3 +10,25 @@ const fullscreenMenuCloseBtn = document.querySelector('#closeMenu');
 fullscreenMenuCloseBtn.addEventListener('click', e => {
     fullscreenMenu.style.display = 'none';
 })
+
+
+
+const dropDownListBtn = document.querySelector('.products__characteristics');
+const dropDownList = document.querySelector('.dropdown-list')
+
+dropDownListBtn.addEventListener('mouseenter', e => {
+        dropDownListBtn.style.backgroundColor = "#E01F3D";
+        dropDownList.style.display = 'flex';
+})
+
+dropDownList.addEventListener('mouseleave', e => {
+        if(e.target == dropDownList){
+            dropDownListBtn.style.backgroundColor = "#9A9A9A";
+            dropDownList.style.display = 'none';
+        }
+})
+
+
+
+
+
