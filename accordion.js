433085bc1eaad699629bf9841feae5856ef9e9c2
Pavel Tyan <paveltyan032@gmail.com>
2021-08-09 11:22:31 +0600

    f

--- accordion.js
+++ accordion.js
@@ -25,9 +25,8 @@ $(document).ready( function (e){
 });
 
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// Дополнительная единица измерения для корректного отображения верстки в браузере на мобильных устройствах
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
